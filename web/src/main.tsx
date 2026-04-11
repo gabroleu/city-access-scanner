62e46a4bc74e4f13f4afeb042943c9bff1e39a5b
Gabriel <rgabrielcs@gmail.com>
2026-04-10 22:16:20 -0400

    marker on the map

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,4 @@
+import 'leaflet/dist/leaflet.css' //here I don't allow the mpa visual to break
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
